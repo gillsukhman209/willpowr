c59e48059f2befc388f660db713d05b3938ed06f
gillsukhman209 <104024595+gillsukhman209@users.noreply.github.com>
2025-08-15 15:03:22 -0700

    basic watch working

---
+++ WillPowr Watch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WillPowr Watch Watch App
+//
+//  Created by Sukhman Singh on 8/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WillPowr Watch Watch App/WillPowr_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WillPowr_WatchApp.swift
+//  WillPowr Watch Watch App
+//
+//  Created by Sukhman Singh on 8/15/25.
+//
+
+import SwiftUI
+
+@main
+struct WillPowr_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
